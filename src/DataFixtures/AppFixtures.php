a45381f64641363f67e928c7a1286507b8f804c7
Paul LOPEZ <p.lopez@alienor.net>
2023-03-23 01:58:06 +0100

    [FEATURE] fixture pour importer des débuts d'articles + images par défaut pour les articles

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        /******
+         * User 
+         ******/
+
+        /******
+         * ARTICLES 
+         ******/
+        // $product = new Product();
+        // $manager->persist($product);
+        $article = new Article();
+        $article->setTitle('Déploiement de la première ligne du ReVE');
+        $article->setSlug('deploiement-de-la-premiere-ligne-du-reve');
+        $article->getAuthor('Paul');
+        $article->setDateCreation(new \DateTime('2023-03-19 16:47:46'));
+        $manager->persist($article);
+        
+        $article = new Article();
+        $article->setTitle('Vélo Score');
+        $article->setSlug('velo-score');
+        $article->getAuthor('Thibaud');
+        $article->setDateCreation(new \DateTime('2023-03-22 00:25:50'));
+        $manager->persist($article);
+        
+        $article = new Article();
+        $article->setTitle('Évolution de la pratique');
+        $article->setSlug('evolution-de-la-pratique');
+        $article->getAuthor('Pierre');
+        $article->setDateCreation(new \DateTime('2023-03-22 23:18:56'));
+        $article->setContent('<p style="text-align:center"><img alt="" src="https://pbs.twimg.com/media/Fl0t7ccWQAADumo?format=jpg&amp;name=large" style="height:375px; width:500px" /></p>');
+        $manager->persist($article);
+        
+        $article = new Article();
+        $article->setTitle('Bilan mi-mandat');
+        $article->setSlug('bilan-mi-mandat');
+        $article->getAuthor('Pierre');
+        $article->setDateCreation(new \DateTime('2023-03-22 23:34:37'));
+        $article->setContent('<p><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-001.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-002.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-003.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-004.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-005.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-006.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-007.jpg" style="height:1125px; width:1125px" /></p>');
+        $manager->persist($article);
+        
+        $article = new Article();
+        $article->setTitle('Radar');
+        $article->setSlug('radar');
+        $article->getAuthor('Loïc');
+        $article->setDateCreation(new \DateTime('2023-03-22 23:39:16'));
+        $article->setContent('<p><img alt="" src="/radar/radar.png" style="height:886px; width:1841px" /></p>');
+        $manager->persist($article);
+
+        $manager->flush();
+    }
+}
