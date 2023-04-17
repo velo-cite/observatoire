dcbd3e898223e5a1d0c23ae8801e49e1d83e0045
Paul LOPEZ <p.lopez@alienor.net>
2023-04-17 22:02:24 +0200

    [FEATURE] data fixture : user + bloc_cms evolution-pratique-after

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\BlocCMS;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,6 +15,12 @@ class AppFixtures extends Fixture
         /******
          * User 
          ******/
+        $user = new User();
+        $user
+            ->setEmail(base64_decode('cGF1bG9wMzMyOTBAZ21haWwuY29t'))
+            ->setName('Paul')
+            ->setPassword('$argon2id$v=19$m=65536,t=4,p=1$7isd9hN3XJU7nQa1+dnqDA$hw5JKZuzU2OOhRfwmIbJ7xM16EZZ+0K+lILUxEC0lmU');
+        $manager->persist($user);
 
         /******
          * ARTICLES 
@@ -20,14 +28,14 @@ class AppFixtures extends Fixture
         $article = new Article();
         $article->setTitle('Déploiement de la première ligne du ReVE');
         $article->setSlug('deploiement-de-la-premiere-ligne-du-reve');
-        $article->getAuthor('Paul');
+        $article->setAuthor('Paul');
         $article->setDateCreation(new \DateTime('2023-03-19 16:47:46'));
         $manager->persist($article);
 
         $article = new Article();
         $article->setTitle('Évolution de la pratique');
         $article->setSlug('evolution-de-la-pratique');
-        $article->getAuthor('Pierre');
+        $article->setAuthor('Pierre');
         $article->setDateCreation(new \DateTime('2023-03-22 23:18:56'));
         $article->setContent('<p style="text-align:center"><img alt="" src="https://pbs.twimg.com/media/Fl0t7ccWQAADumo?format=jpg&amp;name=large" style="height:375px; width:500px" /></p>');
         $manager->persist($article);
@@ -35,7 +43,7 @@ class AppFixtures extends Fixture
         $article = new Article();
         $article->setTitle('Bilan mi-mandat');
         $article->setSlug('bilan-mi-mandat');
-        $article->getAuthor('Pierre');
+        $article->setAuthor('Pierre');
         $article->setDateCreation(new \DateTime('2023-03-22 23:34:37'));
         $article->setContent('<p><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-001.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-002.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-003.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-004.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-005.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-006.jpg" style="height:1125px; width:1125px" /><img alt="" src="/evolution-amenagements/Evolution_Amenagements_Bordeaux-page-007.jpg" style="height:1125px; width:1125px" /></p>');
         $manager->persist($article);
@@ -43,10 +51,15 @@ class AppFixtures extends Fixture
         $article = new Article();
         $article->setTitle('Radar');
         $article->setSlug('radar');
-        $article->getAuthor('Loïc');
+        $article->setAuthor('Loïc');
         $article->setDateCreation(new \DateTime('2023-03-22 23:39:16'));
         $article->setContent('<p><img alt="" src="/radar/radar.png" style="height:886px; width:1841px" /></p>');
         $manager->persist($article);
+
+        $bloc_cms = new BlocCMS();
+        $bloc_cms->setKey('evolution-pratique-after');
+        $bloc_cms->setContent('<p>Evolution de la pratique post graphique</p>');
+        $manager->persist($bloc_cms);
 
         $manager->flush();
     }
